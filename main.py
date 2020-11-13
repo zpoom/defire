cef08e7bb6c814c5afdf6d164af6947ca1489768
zpoom <poom007.sk135@gmail.com>
2020-11-13 13:42:46 +0700

    Read value from sensor

---
+++ main.py
@@ -0,0 +1,17 @@
+import smbus
+import time
+
+address = 0x48
+A0 = 0x40
+
+THRESHOLD = 240
+
+bus = smbus.SMBus(1)
+
+while True:
+    bus.write_byte(address, A0)
+    value = bus.read_byte(address)
+    if value >= THRESHOLD:
+        print('Warning', value)
+        # TODO: broadcast to other pi
+    time.sleep(0.1)
